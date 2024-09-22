a73290b4c00a2774c086b6dd643845fecc0bd6af
mgarciate <mgarciate@gmail.com>
2024-09-23 00:28:46 +0200

    Show info about n8n

--- n8n-workflows-ios/Views/MainView.swift
+++ n8n-workflows-ios/Views/MainView.swift
@@ -21,6 +21,7 @@ struct MainView<ViewModel>: View where ViewModel: MainViewModelProtocol {
     
     @StateObject var viewModel: ViewModel
     @State private var showSettings = false
+    @State private var infoPopoverPresented = false
     @State private var isInactiveOrBackground = false
     
     var body: some View {
@@ -44,6 +45,23 @@ struct MainView<ViewModel>: View where ViewModel: MainViewModelProtocol {
                     } label: {
                         Image(systemName: "gear")
                     }
+                }
+                ToolbarItem(placement: .automatic) {
+                    HStack {
+                        Text("")
+                        NavigationLink {
+                            VStack(alignment: .leading, spacing: 20) {
+                                Text("About n8n")
+                                    .font(.title.bold())
+                                Text("n8n (pronounced n-eight-n) helps you to connect any app with an API with any other, and manipulate its data with little or no code.\n\n- Customizable: highly flexible workflows and the option to build custom nodes.\n- Convenient: use the npm or Docker to try out n8n, or the Cloud hosting option if you want us to handle the infrastructure.\n- Privacy-focused: self-host n8n for privacy and security.")
+                                Spacer()
+                            }
+                            .padding()
+                        } label: {
+                            Image(systemName: "info.circle")
+                        }
+                    }
+                    
                 }
             }
         }
