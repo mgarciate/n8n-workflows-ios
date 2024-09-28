43deb45b4e773db75dce6685cf791466f7b08e67
mgarciate <mgarciate@gmail.com>
2024-09-28 09:40:25 +0200

    Create Onboarding view

---
+++ n8n-workflows-ios/Views/OnboardingView.swift
@@ -0,0 +1,24 @@
+//
+//  OnboardingView.swift
+//  n8n-workflows-ios
+//
+//  Created by mgarciate on 28/9/24.
+//
+
+import SwiftUI
+
+struct ShortcutsHelpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView: View {
+    var body: some View {
+        ShortcutsHelpView()
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
