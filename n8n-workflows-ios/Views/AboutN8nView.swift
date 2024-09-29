e088664d1798d0061b86adc02170f2f3960236aa
mgarciate <mgarciate@gmail.com>
2024-09-29 23:38:48 +0200

    Show onboarding from About n8n screen

--- n8n-workflows-ios/Views/AboutN8nView.swift
+++ n8n-workflows-ios/Views/AboutN8nView.swift
@@ -9,6 +9,8 @@
 import SwiftUI
 
 struct AboutN8nView: View {
+    @State private var isOnboardingPresented = false
+    
     private let n8nUrl = "https://github.com/n8n-io/n8n"
     var body: some View {
         VStack(alignment: .leading) {
@@ -20,12 +22,20 @@ struct AboutN8nView: View {
             Text("n8n (pronounced n-eight-n) helps you to connect any app with an API with any other, and manipulate its data with little or no code.\n\n- Customizable: highly flexible workflows and the option to build custom nodes.\n- Convenient: use the npm or Docker to try out n8n, or the Cloud hosting option if you want us to handle the infrastructure.\n- Privacy-focused: self-host n8n for privacy and security.\n\nCheck the project at: \(n8nUrl)")
             Spacer()
             HStack {
+                Button("Show onboarding") {
+                    isOnboardingPresented.toggle()
+                }
+                .buttonStyle(.borderedProminent)
+                .clipShape(.capsule)
                 Spacer()
                 HStack {
                     Label("\(Bundle.main.appVersion() ?? "")", systemImage: "icloud.and.arrow.down")
                         .font(.caption)
                 }
             }
+        }
+        .sheet(isPresented: $isOnboardingPresented) {
+            OnboardingView()
         }
         .navigationTitle("About n8n")
         .padding()
