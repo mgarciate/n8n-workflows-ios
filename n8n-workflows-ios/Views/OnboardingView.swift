18eee01a719db7bf4f268306df8198991c2a5d8c
mgarciate <mgarciate@gmail.com>
2024-09-30 10:37:44 +0200

    Add image to step 7

--- n8n-workflows-ios/Views/OnboardingView.swift
+++ n8n-workflows-ios/Views/OnboardingView.swift
@@ -64,7 +64,7 @@ struct ShortcutsHelpView: View {
                         .foregroundStyle(.white)
                         .background(.gray)
                         .clipShape(.circle)
-                    Text("Search for and select the **n8n Workflows** app.")
+                    Text("1. Search for and select the **n8n Workflows** app.\n\n2. And also select the action you want: activate or deactivate workflow.")
                 }
                 Image("shortcuts_setup3")
                     .resizable()
@@ -78,14 +78,53 @@ struct ShortcutsHelpView: View {
                         .foregroundStyle(.white)
                         .background(.gray)
                         .clipShape(.circle)
-                    Text("Choose the **n8n Workouts** app")
+                    Text("The next step is to configure the action. To do this:\n\n1. Expand the configuration.\n\n2. Choose the workflow you would like to select.")
                 }
-                HStack {
-                    Text("TODO: Image here")
+                Image("shortcuts_setup4")
+                    .resizable()
+                    .scaledToFit()
+            }
+            Section {
+                HStack(alignment: .top, spacing: 10.0) {
+                    Text("5")
+                        .font(.title2)
                         .padding()
                         .foregroundStyle(.white)
-                        .background(.red)
+                        .background(.gray)
+                        .clipShape(.circle)
+                    Text("**Important:** You can rename the **Shortcut** so that it's more intuitive to launch it by calling Siri. For example, \"Start my custom workout.\"")
                 }
+                Image("shortcuts_setup5")
+                    .resizable()
+                    .scaledToFit()
+            }
+            Section {
+                HStack(alignment: .top, spacing: 10.0) {
+                    Text("6")
+                        .font(.title2)
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.gray)
+                        .clipShape(.circle)
+                    Text("**One more thing:** Additionally, you can use actions to activate or deactivate workflows to include them in your iPhone's automations.")
+                }
+                Image("shortcuts_setup6")
+                    .resizable()
+                    .scaledToFit()
+            }
+            Section {
+                HStack(alignment: .top, spacing: 10.0) {
+                    Text("7")
+                        .font(.title2)
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.gray)
+                        .clipShape(.circle)
+                    Text("For example, you can activate or deactivate your n8n workflows when you arrive at a location (your home?), when you receive an email with a specific subject, when you start an app and want to have a service running on your server and shut it down when the app is closed, when you get in the car, when you connect to a specific Wi-Fi, when your iPhone is running low on battery, etc.")
+                }
+                Image("shortcuts_setup7")
+                    .resizable()
+                    .scaledToFit()
             }
         }
     }
