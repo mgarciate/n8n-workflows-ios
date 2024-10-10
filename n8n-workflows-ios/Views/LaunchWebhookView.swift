61183b7433cbd9e808a688da7170110d78b57281
mgarciate <mgarciate@gmail.com>
2024-10-10 23:08:00 +0200

    Query SwiftData in Launch Webhook view

--- n8n-workflows-ios/Views/LaunchWebhookView.swift
+++ n8n-workflows-ios/Views/LaunchWebhookView.swift
@@ -6,13 +6,85 @@
 //
 
 import SwiftUI
+import SwiftData
+
+struct MyView: View {
+    @Binding var webhookConfiguration: WebhookConfiguration
+    
+    var body: some View {
+        TextField("Name", text: $webhookConfiguration.name)
+    }
+}
 
 struct LaunchWebhookView<ViewModel>: View where ViewModel: LaunchWebhookViewModelProtocol {
+    @Query private var webhookConfigurations: [WebhookConfiguration]
+    @Environment(\.modelContext) private var context
     @Environment(\.dismiss) private var dismiss
     @StateObject var viewModel: ViewModel
+    @State private var selectedConfiguration: WebhookConfiguration
     
     var body: some View {
         List {
+            Section("Manage webhook configurations") {
+                Picker("Configuration", selection: $selectedConfiguration) {
+                    ForEach(webhookConfigurations, id: \.self) { configuration in
+                        VStack {
+                            Text(configuration.name ?? "Default").tag("other")
+                        }
+                        .tag(configuration.id)
+                    }
+                }
+                .pickerStyle(.menu)
+                .disabled(webhookConfigurations.isEmpty)
+                MyView(webhookConfiguration: $selectedConfiguration)
+                Button("New") {
+                    context.insert(
+                        WebhookConfiguration(
+                            webhookId: viewModel.webhook.id,
+                            name: "New",
+                            httpMethod: HTTPMethod.get
+                        )
+                    )
+                    let webhookConfiguration1 = WebhookConfiguration(
+                        webhookId: "webhookId1",
+                        name: "configuration1"
+                    )
+                    let webhookConfiguration2 = WebhookConfiguration(
+                        webhookId: "webhookId1",
+                        name: "configuration2",
+                        webhookAuthType: .basic,
+                        webhookAuthParam1: "basic1",
+                        webhookAuthParam2: "basic2",
+                        queryParams: ["key": "mykey", "value": "myValue"]
+                    )
+                    let webhookConfiguration3 = WebhookConfiguration(
+                        webhookId: "webhookId1",
+                        name: "configuration3",
+                        webhookAuthType: .basic,
+                        webhookAuthParam1: "basic1",
+                        webhookAuthParam2: "basic2",
+                        httpMethod: HTTPMethod.post,
+                        jsonText: "{\"key\": \"value\"}"
+                    )
+                    let webhookConfiguration4 = WebhookConfiguration(
+                        webhookId: "webhookId2",
+                        name: "configuration1",
+                        webhookAuthType: .basic,
+                        webhookAuthParam1: "basic1",
+                        webhookAuthParam2: "basic2",
+                        queryParams: ["key": "mykey2", "value": "myValue2"]
+                    )
+                    
+                    context.insert(webhookConfiguration1)
+                    context.insert(webhookConfiguration2)
+                    context.insert(webhookConfiguration3)
+                    context.insert(webhookConfiguration4)
+                }
+                Button("onDelete") {
+                    context.delete(selectedConfiguration)
+                }
+                .disabled(webhookConfigurations.count <= 1)
+            }
             Section("Webhook request") {
                 Picker("Authentication", selection: $viewModel.webhookAuthenticationType) {
                     ForEach(WebhookAuthType.allCases, id: \.self) { value in
@@ -101,6 +173,27 @@ struct LaunchWebhookView<ViewModel>: View where ViewModel: LaunchWebhookViewMode
                 )
             }
         }
+        .onAppear() {
+            if let selectedConfiguration = webhookConfigurations.first {
+                self.selectedConfiguration = selectedConfiguration
+                print("*** \(selectedConfiguration.name)")
+            } else {
+                context.insert(selectedConfiguration)
+            }
+        }
+    }
+    
+    init(viewModel: ViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+        selectedConfiguration = WebhookConfiguration(
+            webhookId: viewModel.webhook.id,
+            name: "New",
+            httpMethod: HTTPMethod.get
+        )
+        let webhookId = viewModel.webhook.id
+        _webhookConfigurations = Query(filter: #Predicate<WebhookConfiguration> {
+            $0.webhookId == webhookId
+        })
     }
     
     private func delete(at offsets: IndexSet) {
@@ -108,7 +201,8 @@ struct LaunchWebhookView<ViewModel>: View where ViewModel: LaunchWebhookViewMode
     }
 }
 
-#Preview {
+@available(iOS 18.0, *)
+#Preview(traits: .webhookConfigurationSampleData) {
     NavigationStack {
         LaunchWebhookView(viewModel: MockLaunchWebhookViewModel(webhook: Webhook.dummyWebhook))
     }
